daa67178476aa26a293c0042d176a68ce2a6b1c9
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-09-26 16:01:26 -0500

    add browser setting

--- src/test/java/com/sn/playwright/AnnotatedTest.java
+++ src/test/java/com/sn/playwright/AnnotatedTest.java
@@ -4,39 +4,34 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class SimpleTest{
+@UsePlaywright(AnnotatedTest.MyOptions.class)
+public class AnnotatedTest {
 
-    Playwright playwright;
-    Browser browser;
-    Page page;
+    public static class MyOptions implements OptionsFactory {
 
-    @BeforeEach
-    void beforeEach(){
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions()
-                        .setHeadless(false)
-                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
-        );
-        page = browser.newPage();
-    }
-
-    @AfterEach
-    void afterEach(){
-        browser.close();
-        playwright.close();
+        @Override
+        public Options getOptions() {
+            return new Options()
+                    .setHeadless(false)
+                    .setLaunchOptions(
+                            new BrowserType.LaunchOptions()
+                                    .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+                    );
+        }
     }
 
     @Test
-    void shouldShowPageTitle( ){
+    void shouldShowPageTitle(Page page){
         page.navigate("https://practicesoftwaretesting.com");
 
         String title = page.title();
@@ -45,7 +40,7 @@ public class SimpleTest{
     }
 
     @Test
-    void shouldSearchByKeyword(){
+    void shouldSearchByKeyword(Page page){
         page.navigate("https://practicesoftwaretesting.com");
 
         page.locator("[placeholder=Search]").fill("Pliers");
