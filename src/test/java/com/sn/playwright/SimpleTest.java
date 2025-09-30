8da23660e187e95cfa6c7a58de9dbb10b886d844
Sergei Nikiforov <Sergei.Nikiforov.-ND@disney.com>
2025-09-30 11:08:51 -0500

    add searc and filter test

--- src/test/java/com/sn/playwright/SimpleTest.java
+++ src/test/java/com/sn/playwright/SimpleTest.java
@@ -1,10 +1,13 @@
 package com.sn.playwright;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.*;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class SimpleTest{
 
@@ -18,10 +21,11 @@ public class SimpleTest{
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
-                        .setHeadless(true)
+                        .setHeadless(false)
                         .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
         );
         browserContext = browser.newContext();
+        playwright.selectors().setTestIdAttribute("data-test");
     }
 
     @BeforeEach
@@ -40,7 +44,7 @@ public class SimpleTest{
         page.navigate("https://practicesoftwaretesting.com");
 
         String title = page.title();
-        Assertions.assertTrue(title.contains("Practice Software Testing"));
+        org.junit.jupiter.api.Assertions.assertTrue(title.contains("Practice Software Testing"));
 
     }
 
@@ -52,7 +56,26 @@ public class SimpleTest{
         page.locator("button:has-text('Search')").click();
 
         int matchingSearchResult = page.locator(".card").count();
-        Assertions.assertTrue(matchingSearchResult > 0);
+        org.junit.jupiter.api.Assertions.assertTrue(matchingSearchResult > 0);
 
+    }
+
+    @Test
+    void filteringSearchResult( ){
+        page.navigate("https://practicesoftwaretesting.com");
+
+        page.getByPlaceholder("Search").fill("Pliers");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
+
+        assertThat(page.locator(".card")).hasCount(4);
+
+        List<String> productNames = page.getByTestId("product-name").allTextContents();
+        Assertions.assertThat(productNames).allMatch(name -> name.contains("Pliers"));
+
+        Locator outOfStock = page.locator(".card")
+                .filter(new Locator.FilterOptions().setHasText("Out of stock"))
+                .getByTestId("product-name");
+        assertThat(outOfStock).hasCount(1);
+        assertThat(outOfStock).hasText("Long Nose Pliers");
     }
 }
